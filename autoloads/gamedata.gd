fb34e64c82ec88d461964707bcec18c8ef072323
Furkan <TunderEye_00@outlook.com>
2024-02-13 01:07:45 +0300

    Add movement controls to player
    In addition, the player updates its position in the game data.

--- autoloads/gamedata.gd
+++ autoloads/gamedata.gd
@@ -1,1 +1,3 @@
 extends Node2D
+
+var player_position : Vector2
--- entity/player/player.gd
+++ entity/player/player.gd
@@ -1,8 +1,18 @@
 extends CharacterBody2D
 
+@export var movement_speed : int = 250
 var mouse_direction : Vector2
+var movement_direction : Vector2
 
 
 func _process(_delta):
+	Gamedata.player_position = global_position
+	
+	
 	mouse_direction = (get_global_mouse_position() - position).normalized()
 	rotation = lerp_angle(rotation, mouse_direction.angle(), 0.1)
+	
+	
+	movement_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = movement_direction * movement_speed
+	move_and_slide()
