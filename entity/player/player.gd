3f3fd3ac9dea51f740b7d3174622ef4f0b5e6638
Furkan <TunderEye_00@outlook.com>
2024-02-13 01:03:46 +0300

    Add mouse control to player

--- entity/player/player.gd
+++ entity/player/player.gd
@@ -1,1 +1,8 @@
 extends CharacterBody2D
+
+var mouse_direction : Vector2
+
+
+func _process(_delta):
+	mouse_direction = (get_global_mouse_position() - position).normalized()
+	rotation = lerp_angle(rotation, mouse_direction.angle(), 0.1)
