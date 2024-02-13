b9419af72af0b890a2d91701d8ba260ef4fcc59e
Furkan <TunderEye_00@outlook.com>
2024-02-13 13:26:18 +0300

    Make enemy follow player

--- entity/enemy/enemy.gd
+++ entity/enemy/enemy.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
 
+@export var movement_speed : int = 265
 var target_direction : Vector2
 
 
 func _process(_delta):
 	target_direction = (Gamedata.player_position - position).normalized()
+	
+	
 	rotation = lerp_angle(rotation, target_direction.angle(), 0.1)
+	
+	
+	velocity = target_direction * movement_speed
+	move_and_slide()
