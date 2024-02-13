c2f736efd5d1968566f01b709db173c5771bccbc
Furkan <TunderEye_00@outlook.com>
2024-02-13 13:21:55 +0300

    Make enemy look at player

--- entity/enemy/enemy.gd
+++ entity/enemy/enemy.gd
@@ -1,1 +1,8 @@
 extends CharacterBody2D
+
+var target_direction : Vector2
+
+
+func _process(_delta):
+	target_direction = (Gamedata.player_position - position).normalized()
+	rotation = lerp_angle(rotation, target_direction.angle(), 0.1)
